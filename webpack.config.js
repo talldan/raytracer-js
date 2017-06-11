854cb2451c0b389d458b9e56ed870d9e19bf1094
Daniel Richards <daniel.p.richards@gmail.com>
2017-06-11 16:30:29 +0100

    Minor tweaks to webpack config to include html plugin, and specify desired locations of files

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
-const { 
+const {
+  addPlugins,
   createConfig, 
   defineConstants, 
   env, 
   entryPoint, 
   setOutput, 
   sourceMaps 
-} = require('@webpack-blocks/webpack2')
-const babel = require('@webpack-blocks/babel6')
-const devServer = require('@webpack-blocks/dev-server2')
+} = require('@webpack-blocks/webpack2');
+const babel = require('@webpack-blocks/babel6');
+const devServer = require('@webpack-blocks/dev-server2');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    inject: true,
+    template: './src/html/index.html'
+  })
+];
 
 module.exports = createConfig([
-  entryPoint('./src/main.js'),
+  entryPoint('./src/js/main.js'),
   setOutput('./dist/index.js'),
   babel(),
+  addPlugins(plugins),
   defineConstants({
     'process.env.NODE_ENV': process.env.NODE_ENV
   }),
   env('development', [
     devServer(),
-    devServer.proxy({
-      '/api': { target: 'http://localhost:3000' }
-    }),
     sourceMaps()
   ])
-])
+]);
