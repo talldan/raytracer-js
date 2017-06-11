5fbc050e33801919a4463e8c873c8122e34297be
Daniel Richards <daniel.p.richards@gmail.com>
2017-06-11 18:02:39 +0100

    add simple boilerplate for drawing an empty canvas

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-console.log('hi');
+import canvas from './renderers/canvas';
+
+const dimensions = {
+  width: 100,
+  height: 100
+};
+const rootElement = document.getElementById('root');
+const canvasRenderer = canvas(rootElement, dimensions);
+const pixelData = new Array(dimensions.width * dimensions.height * 4);
+
+canvasRenderer(pixelData);
